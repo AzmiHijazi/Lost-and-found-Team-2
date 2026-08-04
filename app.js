2d2f4f99b30081d0fb317558fd1ffc5723b9fba1
AzmiHijazi <202210316@uopstd.edu.jo>
2026-08-04 17:28:53 +0300

    shared nav layout

--- app.js
+++ app.js
@@ -1,18 +1,50 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-
-const bodyParser = require('body-parser');
-const path = require('path');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('index');
+
+    res.redirect('/contact');
+
 });
 
-app.listen(8080);
+app.get('/contact', (req, res) => {
+
+    res.render('contact', {
+        title: 'Contact Us',
+        currentPage: 'contact'
+    });
+
+});
+
+app.post('/contact', (req, res) => {
+
+    console.log(req.body);
+
+    res.redirect('/contact');
+
+});
+
+app.use((req, res) => {
+
+    res.status(404).render('404', {
+        title: 'Page Not Found',
+        currentPage: ''
+    });
+
+});
+
+app.listen(8080, () => {
+
+    console.log('Server is running on port 8080');
+
+});
